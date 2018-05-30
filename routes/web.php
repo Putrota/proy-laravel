30405a42561af937a46ffce057fa4de888d76517
Alexei <alexis.cabrera@gmail.com>
2018-05-30 20:10:01 +0200

    Uso básico de las rutas

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,44 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/', function() {
 
-Route::get('/', function () {
-    return view('welcome');
+	// La función route nos devulve la url de la ruta
+	echo '<a href="">' . route('contactos') . '</a>';
+
+	return 'Hola desde la página de inicio';
+
 });
+
+
+// Podemos definir un alias de contactos para referirnos a el
+// Si cambiamos el nombre de la ruta no nos afectará
+Route::get('contacto', ['as' => 'contactos',function(){
+
+	return 'Sección de contactos';
+
+}]);
+
+
+// Diferentes maneras de recibir variables
+// Variable nombre obligatoria
+Route::get('saludos1/{nombre}', function($nombre) {
+
+	return 'Hola ' . $nombre;
+
+});
+
+
+// Variable nombre opcional con valor por defecto
+Route::get('saludos2/{nombre?}', function($nombre = 'Invitado') {
+
+	return 'Hola ' . $nombre;
+
+});
+
+
+// Variable con validacion
+Route::get('saludos3/{nombre?}', function($nombre = 'Invitado') {
+
+	return 'Hola ' . $nombre;
+
+})->where('nombre', '[A-Za-z]+');
